899816f85a0652935b0b651d42d31ea8c1326f0b
tia <minticecream88@gmail.com>
2022-04-28 00:46:31 +0300

    stability improvements

--- src/main/scala/connection/COSConnection.scala
+++ src/main/scala/connection/COSConnection.scala
@@ -5,7 +5,8 @@ import util.Spark
 
 class COSConnection(conf: Map[String, String]) {
     private val hadoopConf = Spark.sparkSession.sparkContext.hadoopConfiguration
-    private val service = "arseni"
+    private val service = conf("spark.cos.service")
+    private val bucket = conf("spark.cos.bucket")+s".$service/$service"
 
     hadoopConf.set("fs.stocator.scheme.list", "cos")
     hadoopConf.set("fs.cos.impl", "com.ibm.stocator.fs.ObjectStoreFileSystem")
@@ -15,20 +16,21 @@ class COSConnection(conf: Map[String, String]) {
     hadoopConf.set(s"fs.cos.$service.access.key", conf("spark.cos.access.key"))
     hadoopConf.set(s"fs.cos.$service.secret.key", conf("spark.cos.secret.key"))
     hadoopConf.set(s"fs.cos.$service.endpoint",   conf("spark.cos.endpoint"))
+    hadoopConf.set(s"fs.cos.$service.bucket",     conf("spark.cos.bucket"))
 
-    def save(df: DataFrame, bucket: String, filename: String): Boolean = {
+    def save(df: DataFrame, filename: String): Boolean = {
         df.write
           .mode(SaveMode.Overwrite)
           .option("header", "true")
-          .csv(s"cos://$bucket.$service/$filename")
+          .csv(s"cos://$bucket/$filename")
         true
     }
 
-    def read(bucket: String, filename: String): DataFrame = {
-        Spark.sparkSession.read.csv(s"cos://$bucket.$service/$filename")
+    def read(filename: String): DataFrame = {
+        Spark.sparkSession.read.csv(s"cos://$bucket/$filename")
     }
 
-    def listFiles(bucket: String): Array[String] = {
-        Spark.sparkSession.read.textFile(s"cos://$bucket.$service/").inputFiles
+    def listFiles(): Array[String] = {
+        Spark.sparkSession.read.textFile(s"cos://$bucket/").inputFiles
     }
 }
