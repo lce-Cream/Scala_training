8c16c8a0d1d024b15231c125664b7904ed47fb74
tia <minticecream88@gmail.com>
2022-04-28 02:49:08 +0300

    event loop for separate task execution

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,38 +1,105 @@
 import connection.{COSConnection, DB2Connection, LocalConnection, MySQLConnection}
+
 import dataGeneration.DataFrameGenerator
 import dataTransform.Transform.calculateYearSales
+
 import util.DefaultConfig
 import util.Spark
+import util.Environment
+import util.CLIParser
 
 object Main extends App{
-    val config = Spark.sparkSession.conf.getAll // get spark config
+//    val config = Spark.sparkSession.conf.getAll // get spark config
+    val config = Environment.getMap             // get spark config from environment variables
+
     val df = DataFrameGenerator.generateSalesConcise(10) // generate sales DataFrame
-    val df_annual = calculateYearSales(df) // transform it by calculating annual sales
+    val df_annual = calculateYearSales(df)               // transform it by calculating annual sales
 
-    // db2
-    val DB2Connection = new DB2Connection(DefaultConfig.DB2Credentials ++ config) // create a connection
-    if (DB2Connection.write("sales_data", df_annual)){ // write to your connection
-        DB2Connection.read("sales_data").show() // read from the connection and show
-        println(s"Rows inserted: ${DB2Connection.getCount("sales_data")}") // count number of inserted rows
+    // console event loop
+    while (true){
+        val mode = scala.io.StdIn.readLine("$ ")
+
+        mode match {
+            case "db2"    => processDB2()
+            case "cos"    => processCOS()
+            case "mysql"  => processMySQL()
+            case "local"  => processLocal()
+            case "help"   => help()
+            case other    => {
+                println("Unexpected mode: " + other.toString)
+                println(help())
+                sys.exit(1)
+            }
+        }
     }
 
-    // cos
-    val COSConnection = new COSConnection(DefaultConfig.COSCredentials ++ config) // create a connection
-    if (COSConnection.save(df_annual, "arseni-storage", "sales.csv")){ // write to the connection
-        COSConnection.read("arseni-storage", "sales.csv").show // read and show
-        COSConnection.listFiles("arseni-storage").foreach(println) // list all files in cos
+    def help(): String = {
+        """
+          |Choose mode to run process.
+          |Mods available: db2, cos, mysql, local.
+          |""".stripMargin
     }
 
-    //mysql
-    val MySQLConnection = new DB2Connection(DefaultConfig.MySQLCredentials ++ config) // create a connection
-    if (MySQLConnection.write("sales_data", df_annual)){ // write to your connection
-        MySQLConnection.read("sales_data").show() // read from connection and show
-        println(s"Rows inserted: ${MySQLConnection.getCount("sales_data")}") // count number of inserted rows
+    def processDB2(): Boolean = {
+        try{
+            val DB2Connection = new DB2Connection(DefaultConfig.DB2Credentials ++ config) // create a connection
+            DB2Connection.write("sales_data", df_annual)                                  // write to your connection
+            DB2Connection.read("sales_data").show()                                       // read from the connection and show
+            println(s"Rows inserted: ${DB2Connection.getCount("sales_data")}")            // count number of inserted rows
+            true
+        }
+        catch {
+            case e: Exception => {
+                println(e.getMessage)
+                false
+            }
+        }
     }
 
-    // local
-    val LocalConnection = new LocalConnection(DefaultConfig.LocalCredentials ++ config) // create a connection
-    if (LocalConnection.save(df, "test.csv", "overwrite")){ // write to the connection
-        LocalConnection.read("test.csv").show // read and show from the connection
+    def processCOS(): Boolean = {
+        try{
+            val COSConnection = new COSConnection(DefaultConfig.COSCredentials ++ config) // create a connection
+            COSConnection.save(df_annual, "sales.csv")                                    // write to the connection
+            COSConnection.read("sales.csv").show                                          // read and show
+            COSConnection.listFiles().foreach(println)                                    // list all files in cos
+            true
+        }
+        catch {
+            case e: Exception => {
+                println(e.getMessage)
+                false
+            }
+        }
+    }
+
+    def processMySQL(): Boolean = {
+        try{
+            val MySQLConnection = new DB2Connection(DefaultConfig.MySQLCredentials ++ config) // create a connection
+            MySQLConnection.write("sales_data", df_annual)                                    // write to your connection
+            MySQLConnection.read("sales_data").show()                                         // read from connection and show
+            println(s"Rows inserted: ${MySQLConnection.getCount("sales_data")}")              // count number of inserted rows
+            true
+        }
+        catch {
+            case e: Exception => {
+                println(e.getMessage)
+                false
+            }
+        }
+    }
+
+    def processLocal(): Boolean = {
+        try{
+            val LocalConnection = new LocalConnection(DefaultConfig.LocalCredentials ++ config) // create a connection
+            LocalConnection.save(df, "test.csv", "overwrite")                                   // write to the connection
+            LocalConnection.read("test.csv").show                                               // read and show from the connection
+            true
+        }
+        catch {
+            case e: Exception => {
+                println(e.getMessage)
+                false
+            }
+        }
     }
 }
