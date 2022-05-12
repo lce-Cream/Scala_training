9286aaa1122a158a8930c7c902262f46a114570f
tia <minticecream88@gmail.com>
2022-05-13 00:49:41 +0300

    tests for cli parser

---
+++ src/test/scala/CLITest.scala
@@ -0,0 +1,37 @@
+import org.scalatest.wordspec.AnyWordSpec
+import util.CLIParser
+
+class CLITest extends AnyWordSpec{
+    "CLI" should {
+        "return map with parsed arguments" in {
+            assert(
+                CLIParser.parse(Array("-m","db2","-a","read","-n","10"))
+                  == Map[String, String](
+                    "mode"   -> "db2",
+                    "action" -> "read",
+                    "number" -> "10",
+                )
+            )
+            assert(
+                CLIParser.parse(Array("--mode","cos","--action","write","--number","100"))
+                  == Map[String, String](
+                    "mode"   -> "cos",
+                    "action" -> "write",
+                    "number" -> "100",
+                )
+            )
+        }
+
+        "throw exception when number of arguments is not equal to 3" in {
+            assertThrows[Exception](CLIParser.parse(Array("-m","db2","-a","read")))
+            assertThrows[Exception](CLIParser.parse(Array("-m","db2","-a","read","-n","-k","arg")))
+            assertThrows[Exception](CLIParser.parse(Array()))
+        }
+
+        "throw exception if argument's value is incorrect" in {
+            assertThrows[Exception](CLIParser.parse(Array("-m","db2","-a","read","-n","ten")))
+            assertThrows[Exception](CLIParser.parse(Array("-m","db2","-a","delete","-n","10")))
+            assertThrows[Exception](CLIParser.parse(Array("-m","postgres","-a","write","-n","10")))
+        }
+    }
+}
